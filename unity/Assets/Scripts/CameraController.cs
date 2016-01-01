f9813e13f5aca11d90ccf80bf4f2395be7fac32c
EZ <psizzlepip@yahoo.com>
2016-01-01 15:59:30 -0800

    project push

---
+++ unity/Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour 
+{
+	public GameObject player;
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () 
+	{
+		offset = transform.position;
+	}
+	
+	// LateUpdate is best for follow cameras, procederal animation, and gathering last known states.
+	void LateUpdate () 
+	{
+		transform.position = player.transform.position + offset;
+	}
+}
